08f60fda42cdad5bc3c3f593e9bd1fd271896ccf
Mathew Bollinger <matthewrb990@gmail.com>
2023-06-08 22:15:16 -0400

    Pulled Pokemon Games From API Into Dropdown
    Moved API calls to Home component and added API call to get pokemon games and populated dropdown

--- pokemonclient/pokemon-team-builder-ux/src/App.js
+++ pokemonclient/pokemon-team-builder-ux/src/App.js
@@ -1,37 +1,15 @@
-//import logo from './logo.svg';
 import './App.css';
-import api from './api/axiosConfig';
-import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
 import Home from './components/home/Home';
 
 function App() {
 
-  const [pokemon, setPokemon] = useState([]); // Must have [] as an argument as will get an undefined error otherwise
-
-  const getPokemon = async () => {
-
-    try {  
-      const response = await api.get("/api/samplepokemon");
-      console.log(response.data);
-      setPokemon(response.data);
-    } 
-    catch (err) {  
-      console.log(err);
-    }
-     
-  }
-
-  useEffect(() => {
-    getPokemon();
-  },[])
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home pokemon={pokemon}/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
         </Route>
       </Routes>
     </div>
--- pokemonclient/pokemon-team-builder-ux/src/components/home/Home.js
+++ pokemonclient/pokemon-team-builder-ux/src/components/home/Home.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import Select from 'react-select';
-import { useState } from "react";
+import api from '../../api/axiosConfig';
+import {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from "react-bootstrap";
 import PokemonSlot from '../pokemonSlot/PokemonSlot';
 
-const Home = ({pokemon}) => {
-  const options = [
-    { value: 'region1', label: 'Region 1' },
-    { value: 'region2', label: 'Region 2' },
-    { value: 'region3', label: 'Region 3' }
-  ];
+const Home = () => {
+  const [pokemon, setPokemon] = useState([]); // Must have [] as an argument as will get an undefined error otherwise
+
+  const getPokemon = async () => {
+
+    try {  
+      const response = await api.get("/api/samplepokemon");
+      console.log(response.data);
+      setPokemon(response.data);
+    } 
+    catch (err) {  
+      console.log(err);
+    }
+     
+  }
+
+  const [pokemonGame, setPokemonGame] = useState([]); // Must have [] as an argument as will get an undefined error otherwise
+
+  const getPokemonGame = async () => {
+
+    try {  
+      const pokedexResponse = await api.get("/api/pokedex");
+      console.log(pokedexResponse.data);
+      setPokemonGame(pokedexResponse.data);
+    } 
+    catch (err) {  
+      console.log(err);
+    }
+     
+  }
+
+  useEffect(() => {
+    getPokemon();
+    getPokemonGame();
+  },[])
+
+
+  const gameOptions = [];
+  for (let i = 0; i < pokemonGame.length; i++) {
+    gameOptions.push({
+      value: i.toString(),
+      label: pokemonGame[i]
+    })
+  }
 
   const [selected, setSelected] = useState(null);
 
@@ -21,8 +60,8 @@ const Home = ({pokemon}) => {
 
   return (
     <Container fluid="md">
-      <Row>
-        <Col className="justify-content-md-left" xs={12} md={4}>
+      <Row style={{marginTop: '2rem'}}>
+        <Col xs={12} md={4}>
           {
             pokemon.map((singlePokemon) => {
             return(
@@ -35,7 +74,9 @@ const Home = ({pokemon}) => {
           }
         </Col>
         <Col xs={12} md={8}>
-          <div><Select options={options} onChange={handleChange} autoFocus={true} /></div>
+          <Col className="mx-auto" md={6}>
+            <div><Select options={gameOptions} onChange={handleChange} autoFocus={true} /></div>
+          </Col>
         </Col>
       </Row>
     </Container>
