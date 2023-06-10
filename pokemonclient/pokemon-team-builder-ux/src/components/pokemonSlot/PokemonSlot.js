fb372da844c4558860dc066351902d7279e5a005
Mathew Bollinger <matthewrb990@gmail.com>
2023-06-10 00:19:30 -0400

    Remove Button Implemented
    Added logic to clear out the box when the remove button is pressed

--- pokemonclient/pokemon-team-builder-ux/src/components/pokemonSlot/PokemonSlot.js
+++ pokemonclient/pokemon-team-builder-ux/src/components/pokemonSlot/PokemonSlot.js
@@ -1,15 +1,34 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
 import { Box } from '@mui/material';
 import { Button } from 'react-bootstrap';
 
+
 const PokemonSlot = ({singlePokemon}) => {
+
+  const [pokemonInfo, setPokemonInfo] = useState(null);
+
+  const getPokemonInfo = () => {
+    console.log(`Pokemon Info:`, singlePokemon);
+    setPokemonInfo(singlePokemon);
+  };
+
+  const handleClick = () => {
+    setPokemonInfo(null);
+    console.log("Box Cleared");
+  };
+
+  useEffect(() => {
+    getPokemonInfo();
+  },[])
+
   return (
     <div>
       <Box sx={{ p: 2, border: '1px dashed grey' }}>
-        <div>{singlePokemon}</div>
+        <div>{pokemonInfo}</div>
       </Box>
       <Button variant="success">Add</Button>
-      <Button variant="danger">Remove</Button>
+      <Button variant="danger" onClick={handleClick}>Remove</Button>
     </div>
     
   )
