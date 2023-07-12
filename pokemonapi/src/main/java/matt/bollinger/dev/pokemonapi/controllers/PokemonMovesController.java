39431c19dab33bb3522cccd393da7fa3ed58f907
Mathew Bollinger <matthewrb990@gmail.com>
2023-07-12 15:29:12 -0400

    Created Move API Endpoints and List in UI
    Created Backend API endpoint to both list the move names and their details. The user interface was modified to list the details below a dropdown.

---
+++ pokemonapi/src/main/java/matt/bollinger/dev/pokemonapi/controllers/PokemonMovesController.java
@@ -0,0 +1,47 @@
+package matt.bollinger.dev.pokemonapi.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import matt.bollinger.dev.pokemonapi.services.PokemonMovesService;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
+@RequestMapping("/api/moves")
+public class PokemonMovesController {
+    
+    @Autowired
+    private PokemonMovesService movesService;
+
+    @GetMapping
+    public ResponseEntity<List<String>> getPokemonTypesNoArgs() {
+        return new ResponseEntity<List<String>>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("/{pokemon}")
+    public ResponseEntity<List<List<String>>> getPokemonMoves(@PathVariable String pokemon) {
+        try {
+            return new ResponseEntity<List<List<String>>>(movesService.getPokemonMoves(pokemon), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<List<String>>>(new ArrayList<>(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{move}/{pokemonGame}")
+    public ResponseEntity<List<String>> getPokemonMovesTwoArgs(@PathVariable String move, @PathVariable String pokemonGame) {
+        try {
+            return new ResponseEntity<List<String>>(movesService.getPokemonMoves(move, pokemonGame), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<String>>(new ArrayList<>(), HttpStatus.NOT_FOUND);
+        }
+    }
+}
