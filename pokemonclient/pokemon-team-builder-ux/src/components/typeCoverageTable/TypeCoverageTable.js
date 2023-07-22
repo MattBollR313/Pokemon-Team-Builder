2882f119eafa3eb37d0702b01fe2a56e22a01d3b
Mathew Bollinger <matthewrb990@gmail.com>
2023-07-22 12:19:32 -0400

    Stylized Type Coverage Table
    Improved style of table with borders and default values for the Pokemon names

--- pokemonclient/pokemon-team-builder-ux/src/components/typeCoverageTable/TypeCoverageTable.js
+++ pokemonclient/pokemon-team-builder-ux/src/components/typeCoverageTable/TypeCoverageTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState, useEffect} from 'react';
+import {useEffect} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -51,32 +51,32 @@ const TypeCoverageTable = ({pokemon1Types, pokemon2Types, pokemon3Types, pokemon
     return (
         <div>
             <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} size="medium" aria-label="simple table">
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="simple table">
                     <TableHead>
                     <TableRow>
                         <TableCell></TableCell>
-                        {pokemon1Types.length !== 0 ? <TableCell align="center">{pokemon1Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
-                        {pokemon2Types.length !== 0 ? <TableCell align="center">{pokemon2Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
-                        {pokemon3Types.length !== 0 ? <TableCell align="center">{pokemon3Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
-                        {pokemon4Types.length !== 0 ? <TableCell align="center">{pokemon4Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
-                        {pokemon5Types.length !== 0 ? <TableCell align="center">{pokemon5Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
-                        {pokemon6Types.length !== 0 ? <TableCell align="center">{pokemon6Types[0]}</TableCell> : <TableCell align="center">Pokemon</TableCell> }
+                        {pokemon1Types.length !== 0 ? <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">{pokemon1Types[0]}</TableCell> : <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">--1--</TableCell> }
+                        {pokemon2Types.length !== 0 ? <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{pokemon2Types[0]}</TableCell> : <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">--2--</TableCell> }
+                        {pokemon3Types.length !== 0 ? <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{pokemon3Types[0]}</TableCell> : <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">--3--</TableCell> }
+                        {pokemon4Types.length !== 0 ? <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{pokemon4Types[0]}</TableCell> : <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">--4--</TableCell> }
+                        {pokemon5Types.length !== 0 ? <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{pokemon5Types[0]}</TableCell> : <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">--5--</TableCell> }
+                        {pokemon6Types.length !== 0 ? <TableCell align="center">{pokemon6Types[0]}</TableCell> : <TableCell align="center">--6--</TableCell> }
                     </TableRow>
                     </TableHead>
                     <TableBody>
                     {rows.map((row) => (
                         <TableRow
                         key={row.type}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        sx={{ '&:last-child td, &:last-child th': { borderBottom: 0 } }}
                         >
                             <TableCell component="th" scope="row" align="center">
                                 <img className="table-type-icon" src={row[0]} alt="Type" />
                             </TableCell>
-                            <TableCell align="center">{row[1]}</TableCell>
-                            <TableCell align="center">{row[2]}</TableCell>
-                            <TableCell align="center">{row[3]}</TableCell>
-                            <TableCell align="center">{row[4]}</TableCell>
-                            <TableCell align="center">{row[5]}</TableCell>
+                            <TableCell sx={{ borderLeft: "1px solid #d3d3d3", borderRight: "1px solid #d3d3d3" }} align="center">{row[1]}</TableCell>
+                            <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{row[2]}</TableCell>
+                            <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{row[3]}</TableCell>
+                            <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{row[4]}</TableCell>
+                            <TableCell sx={{ borderRight: "1px solid #d3d3d3" }} align="center">{row[5]}</TableCell>
                             <TableCell align="center">{row[6]}</TableCell>
                         </TableRow>
                     ))}
