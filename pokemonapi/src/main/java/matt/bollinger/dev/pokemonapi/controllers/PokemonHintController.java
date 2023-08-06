d55ba84344675ef4bd9bf12e6a6bdf7d4786d40d
Mathew Bollinger <matthewrb990@gmail.com>
2023-08-05 21:37:13 -0400

    Created Hints API Controller
    Created the controller class for the Hints API Endpoint alongside a service class and two additional classes to provide hints for types and evolution status

---
+++ pokemonapi/src/main/java/matt/bollinger/dev/pokemonapi/controllers/PokemonHintController.java
@@ -0,0 +1,38 @@
+package matt.bollinger.dev.pokemonapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import matt.bollinger.dev.pokemonapi.services.PokemonHintService;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
+@RequestMapping("/api/hints")
+public class PokemonHintController {
+
+    @Autowired
+    private PokemonHintService hintService;
+
+    @GetMapping
+    public ResponseEntity<List<String>> getSinglePokemonTeam(@RequestHeader("pokemon-1") List<String> pokemon1, 
+        @RequestHeader("pokemon-2") List<String> pokemon2, 
+        @RequestHeader("pokemon-3") List<String> pokemon3, 
+        @RequestHeader("pokemon-4") List<String> pokemon4, 
+        @RequestHeader("pokemon-5") List<String> pokemon5, 
+        @RequestHeader("pokemon-6") List<String> pokemon6) {
+        try {
+            return new ResponseEntity<List<String>>(hintService.getHints(pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<List<String>>(List.of(e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
---
+++ pokemonapi/src/main/java/matt/bollinger/dev/pokemonapi/services/PokemonHintService.java
@@ -0,0 +1,102 @@
+package matt.bollinger.dev.pokemonapi.services;
+
+import java.util.*;
+
+import org.springframework.stereotype.Service;
+
+import matt.bollinger.dev.pokemonapi.services.hints.*;
+
+@Service
+public class PokemonHintService {
+    
+    public List<String> getHints(List<String> pokemon1, List<String> pokemon2, List<String> pokemon3,
+            List<String> pokemon4, List<String> pokemon5, List<String> pokemon6) {
+        List<String> types = getTypes(pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6);
+        Map<String, Integer> typeHints = TypeHints.getTypeHints(types);
+        List<List<String>> evolutionStatuses = getEvolutionStatuses(pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6);
+        Map<String, Integer> evolutionStatusHints = EvolutionStatusHints.getEvolutionStatusHints(evolutionStatuses);
+        Map<String, Integer> allWeightedHints = new HashMap<>();
+        allWeightedHints.putAll(typeHints);
+        allWeightedHints.putAll(evolutionStatusHints);
+        List<String> finalHints = getFinalHints(allWeightedHints);
+        return finalHints;
+    }
+
+    public List<String> getTypes(List<String> pokemon1, List<String> pokemon2, List<String> pokemon3,
+            List<String> pokemon4, List<String> pokemon5, List<String> pokemon6) {
+        List<String> types = new ArrayList<>();
+        if (!pokemon1.isEmpty()) {
+            types.add(pokemon1.get(7));
+            if (!pokemon1.get(8).equals(""))
+                types.add(pokemon1.get(8));
+        }
+        if (!pokemon2.isEmpty()) {
+            types.add(pokemon2.get(7));
+            if (!pokemon2.get(8).equals(""))
+                types.add(pokemon2.get(8));
+        }
+        if (!pokemon3.isEmpty()) {
+            types.add(pokemon3.get(7));
+            if (!pokemon3.get(8).equals(""))
+                types.add(pokemon3.get(8));
+        }
+        if (!pokemon4.isEmpty()) {
+            types.add(pokemon4.get(7));
+            if (!pokemon4.get(8).equals(""))
+                types.add(pokemon4.get(8));
+        }
+        if (!pokemon5.isEmpty()) {
+            types.add(pokemon5.get(7));
+            if (!pokemon5.get(8).equals(""))
+                types.add(pokemon5.get(8));
+        }
+        if (!pokemon6.isEmpty()) {
+            types.add(pokemon6.get(7));
+            if (!pokemon6.get(8).equals(""))
+                types.add(pokemon6.get(8));
+        }
+        return types;
+    }
+
+    public List<List<String>> getEvolutionStatuses(List<String> pokemon1, List<String> pokemon2, List<String> pokemon3,
+            List<String> pokemon4, List<String> pokemon5, List<String> pokemon6) {
+        List<List<String>> evolutionStatuses = new ArrayList<>();
+        evolutionStatuses.add(Arrays.asList(pokemon1.get(0), pokemon1.get(9)));
+        evolutionStatuses.add(Arrays.asList(pokemon2.get(0), pokemon2.get(9)));
+        evolutionStatuses.add(Arrays.asList(pokemon3.get(0), pokemon3.get(9)));
+        evolutionStatuses.add(Arrays.asList(pokemon4.get(0), pokemon4.get(9)));
+        evolutionStatuses.add(Arrays.asList(pokemon5.get(0), pokemon5.get(9)));
+        evolutionStatuses.add(Arrays.asList(pokemon6.get(0), pokemon6.get(9)));
+        return evolutionStatuses;
+    }
+
+    private List<String> getFinalHints(Map<String, Integer> allHints) {
+        List<Map.Entry<String, Integer> > hintList = new ArrayList<Map.Entry<String, Integer> >(allHints.entrySet());
+ 
+        Collections.sort(hintList, new Comparator<Map.Entry<String, Integer> >() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2)
+            { 
+                return (o2.getValue()).compareTo(o1.getValue()); 
+            }
+        });
+         
+        /*Map<String, Integer> temp = new HashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : hintList) {
+            temp.put(aa.getKey(), aa.getValue());
+        }*/
+
+        List<String> finalHints = new ArrayList<>();
+        if (hintList.size() <= 5) {
+            for (int i = 0; i < hintList.size(); i++) {
+                finalHints.add(hintList.get(i).getKey());
+            }
+        } else {
+            for (int i = 0; i < 5; i++) {
+                finalHints.add(hintList.get(i).getKey());
+            }
+        }
+
+        return finalHints;
+    }
+
+}
