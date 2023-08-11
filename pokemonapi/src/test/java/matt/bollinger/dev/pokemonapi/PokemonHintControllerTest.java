b5966552c0c1a9bf0c14f0a7f6055f0fd79e5fab
Mathew Bollinger <matthewrb990@gmail.com>
2023-08-10 23:03:05 -0400

    Added Test Cases for Move Hint Class
    Added test cases to test for when the MoveHints class is called

--- pokemonapi/src/test/java/matt/bollinger/dev/pokemonapi/PokemonHintControllerTest.java
+++ pokemonapi/src/test/java/matt/bollinger/dev/pokemonapi/PokemonHintControllerTest.java
@@ -199,4 +199,110 @@ public class PokemonHintControllerTest {
 			.contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
+	@Test
+	public void testOneMoveNotHigherBaseStatRequest() throws Exception {
+		String[] allHints = {"Empoleon has at least one attack move which does not match its higher base stat. Consider picking different moves", "Six Pokemon have not been added to your team. Consider filling out the remaining team"};
+
+		Mockito.when(service.getHints(
+			List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))).thenReturn(Arrays.asList(allHints));
+
+		mockMvc.perform(MockMvcRequestBuilders.get(endPoint)
+			.header("pokemon-1", 
+				List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"))
+			.header("pokemon-2", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-3", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-4", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-5", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-6", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testMultiplePokemonMoveNotHigherBaseStatRequest() throws Exception {
+		String[] allHints = {"Empoleon, Infernape have at least one attack move which does not match their higher base stat. Consider picking different moves", "Six Pokemon have not been added to your team. Consider filling out the remaining team"};
+
+		Mockito.when(service.getHints(
+			List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"), 
+			List.of("Infernape", "100", "120", "100", "100", "100", "100", "Fire", "Fighting", "Final Stage", 
+				"Fire Punch", "Fire", "Physical", "Brick Break", "Fighting", "Physical", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))).thenReturn(Arrays.asList(allHints));
+
+		mockMvc.perform(MockMvcRequestBuilders.get(endPoint)
+			.header("pokemon-1", 
+				List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"))
+			.header("pokemon-2", 
+				List.of("Infernape", "100", "120", "100", "100", "100", "100", "Fire", "Fighting", "Final Stage", 
+				"Fire Punch", "Fire", "Physical", "Brick Break", "Fighting", "Physical", "Flash Cannon", "Steel", "Special", "Peck", "Flying", "Physical"))
+			.header("pokemon-3", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-4", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-5", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-6", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testOnePokemonMultipleMovesDifferentTypeRequest() throws Exception {
+		String[] allHints = {"Empoleon has at least three moves which do not match one of the Pokemon's types. Consider picking different moves", "Six Pokemon have not been added to your team. Consider filling out the remaining team"};
+
+		Mockito.when(service.getHints(
+			List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Blizzard", "Ice", "Special", "Giga Drain", "Grass", "Special"), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))).thenReturn(Arrays.asList(allHints));
+
+		mockMvc.perform(MockMvcRequestBuilders.get(endPoint)
+			.header("pokemon-1", 
+				List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Blizzard", "Ice", "Special", "Giga Drain", "Grass", "Special"))
+			.header("pokemon-2", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-3", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-4", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-5", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-6", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testMultiplePokemonMultipleMovesDifferentTypeRequest() throws Exception {
+		String[] allHints = {"Empoleon, Infernape have at least three moves which do not match one of the Pokemons' types. Consider picking different moves", "Six Pokemon have not been added to your team. Consider filling out the remaining team"};
+
+		Mockito.when(service.getHints(
+			List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Blizzard", "Ice", "Special", "Giga Drain", "Grass", "Special"), 
+			List.of("Infernape", "100", "120", "100", "100", "100", "100", "Fire", "Fighting", "Final Stage", 
+				"Fire Punch", "Fire", "Physical", "Shadow Claw", "Ghost", "Physical", "Thunder Punch", "Electric", "Physical", "Peck", "Flying", "Physical"), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""), 
+			List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))).thenReturn(Arrays.asList(allHints));
+
+		mockMvc.perform(MockMvcRequestBuilders.get(endPoint)
+			.header("pokemon-1", 
+				List.of("Empoleon", "100", "100", "100", "120", "100", "100", "Water", "Steel", "Final Stage", 
+				"Surf", "Water", "Special", "Ice Beam", "Ice", "Special", "Blizzard", "Ice", "Special", "Giga Drain", "Grass", "Special"))
+			.header("pokemon-2", 
+				List.of("Infernape", "100", "120", "100", "100", "100", "100", "Fire", "Fighting", "Final Stage", 
+				"Fire Punch", "Fire", "Physical", "Shadow Claw", "Ghost", "Physical", "Thunder Punch", "Electric", "Physical", "Peck", "Flying", "Physical"))
+			.header("pokemon-3", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-4", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-5", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.header("pokemon-6", List.of("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+			.contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
 }
